3f8f90e25fa30fa1df6c84c371d6c3b22b6f559a
mdreffet <44909650+mdreffet@users.noreply.github.com>
2018-11-09 16:29:56 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'skinManager', 'userSettings', 'pluginManager', 'brow
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Test Theme';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'test.theme';
 
         var clockInterval;
         self.load = function () {
